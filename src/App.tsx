a37350e90cb925827e7e2b20e04be0a06ac36063
Adriano Rocha <adrianorocha.dev@gmail.com>
2026-01-02 13:48:48 -0300

    Removido component: testimonials, a pedido do cliente

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Header from './components/Header/Header'
 import Hero from './components/Hero/Hero'
 import About from './components/About/About'
 import TourSection from './components/Tours/TourSection'
-import Testimonials from './components/Testimonials/Testimonials'
+
 import Contact from './components/Contact/Contact'
 import Footer from './components/Footer/Footer'
 import Partners from './components/Partners/Partners'
@@ -29,7 +29,7 @@ function HomePage() {
       <About />
       <TourSection ref={toursRef} />
       <Partners />
-      <Testimonials />
+
       <Contact />
       <WhatsAppButton />
       <Footer />
